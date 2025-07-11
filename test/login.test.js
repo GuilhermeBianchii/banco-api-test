ba6f882e075848bb6be3c917b9676f84e7ca9464
GUILHERME BIANCHI <Guimultimarcas18@gmail.com>
2025-07-11 08:59:21 -0300

    Iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+
+describe('Login Tests',() =>{
+    describe('Post /login', () => {
+        it('Deve retornar 200 com um token em string quando as credenciais estão corretas', async() => {
+            const resposta =  await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send( {"username": "gui.bianchi",
+                        "senha": "123456"})
+                expect(resposta.status).to.equal(200);
+                expect(resposta.body.token).to.be.a('string');
+    })
+})
+})
